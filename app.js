7d088adda5e0b82a8ed6ab0004812c5f3b246cfe
Lukasz <lukaszq10@gmail.com>
2021-07-06 18:54:27 +0200

    Nowy endpoint

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ app.get("/", (req, res) => {
     res.send("<h1>Test</h1>")
 })
 
+app.get("/new", (req, res) => {
+    res.send("<h1>new</h1>")
+})
+
 app.listen(3000, () => {
     console.log("serwer")
 })
